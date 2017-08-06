727bf2bfeaa656bfa9ae42ad4176244e7cc7279d
Gulab Chopra <c.gulab@gmail.com>
2017-08-07 01:33:38 +0530

    code and ui

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,98 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items: Array<{title: string, checked: string, editmode: boolean, position: number}>;
+  deleteditems: Array<{title: string, checked: string, editmode: boolean, position: number}>;
+  newitem: string;
+  newvalue: string;
+  
+  constructor(public navCtrl: NavController, private storage: Storage) {
+  	this.items = [];
+    this.deleteditems = [];
+    let that = this;
+    this.storage.get('items').then(function (result){
+      if (result){
+        that.items = JSON.parse(result);
+      }
+    }, function (err) {
+      console.error(err);
+    });
 
-  constructor(public navCtrl: NavController) {
-
+    this.storage.get('deleteditems').then(function (result) {
+      if (result) {
+        that.deleteditems = JSON.parse(result);
+      }
+    }, function (err) {
+      console.log(err);
+    })
   }
 
+  saveToStorage() {
+    this.storage.set('items', JSON.stringify(this.items));
+  }
+
+  saveDeletedToStorage(){
+    this.storage.set('deleteditems', JSON.stringify(this.deleteditems));
+  }
+
+  additem (){
+    let newitemObj = { title: this.newitem, checked: "", editmode: false, position: this.items.length + 1};
+    this.items.push(newitemObj);
+    this.newitem = '';
+
+    this.saveToStorage();
+  }
+
+  enableEditing (item) {
+    this.newvalue = item.title;
+    item.editmode= true;
+  }
+
+  edititem (item) {
+    debugger;
+    var indexOfItem = this.items.indexOf(item);
+    this.items[indexOfItem].title = this.newvalue;
+    this.items[indexOfItem].editmode = false;
+
+    this.saveToStorage();
+  }
+
+  deleteitem (item) {
+    var indexOfItem = this.items.indexOf(item);
+    if (indexOfItem > -1) {
+      this.deleteditems.push(this.items[indexOfItem]);
+      this.items.splice(indexOfItem, 1);
+
+      this.saveToStorage();
+      this.saveDeletedToStorage();
+    }
+  }
+
+  reorderItems(indexes) {
+    let element = this.items[indexes.from];
+    this.items.splice(indexes.from, 1);
+    this.items.splice(indexes.to, 0, element);
+
+    this.positionitems();
+    this.saveToStorage();
+  }
+
+  positionitems (){
+    for (var i = 0; i < this.items.length; i++) {
+      this.items[i].position = i + 1;
+    }
+  }
+
+  checkitem (item) {
+    debugger;
+    item.checked = (item.checked === "") ? "checked" : "";
+
+    this.saveToStorage();
+  }
 }
--- src/pages/list/list.ts
+++ src/pages/list/list.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-list',
@@ -8,30 +9,57 @@ import { NavController, NavParams } from 'ionic-angular';
 export class ListPage {
   selectedItem: any;
   icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{title: string, checked: string}>;
+  homeitems: Array<{title: string, checked: string, editmode: boolean, position: number}>;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private storage: Storage) {
     this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+    this.homeitems = [];
+    let that = this;
+
+    this.storage.get('deleteditems').then(function (result) {
+      debugger;
+      if (result){
+        that.items = JSON.parse(result);
+      }
+    }, function (err) {
+      console.log(err);
+    });
+
+    this.storage.get('items').then(function (result){
+      if (result){
+        that.homeitems = JSON.parse(result);
+      }
+    }, function (err) {
+      console.error(err);
+    });
+  }
+
+  saveDeletedToStorage(){
+    this.storage.set('deleteditems', JSON.stringify(this.items));
+  }
+
+  saveHomeItemsToStorage(){
+    this.storage.set('items', JSON.stringify(this.homeitems));
+  }
+
+  removeitem (item) {
+    var indexOfItem = this.items.indexOf(item);
+    if (indexOfItem > -1) {
+      this.items.splice(indexOfItem, 1);
+
+      this.saveDeletedToStorage();
     }
   }
 
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  restoreitem (item) {
+    this.homeitems.push({ title: item.title, checked: "", editmode: false, position: this.homeitems.length + 1});
+    this.removeitem(item);
+
+    this.saveHomeItemsToStorage();
+  }
+
+  clearitem (item) {
+    this.removeitem(item);
   }
 }
