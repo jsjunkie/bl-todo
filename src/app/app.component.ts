9cbe3e21cf7e684aebdb12f740126b61642b9bc3
Gulab Chopra <c.gulab@gmail.com>
2017-08-17 17:12:12 +0530

    title changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,7 +21,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
+      { title: 'To-do List', component: HomePage },
       { title: 'Deleted', component: ListPage }
     ];
   }
